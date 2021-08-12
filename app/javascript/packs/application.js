3583b703464a1540c337e335e0b38e543447536c
NiravDarji0612 <darjinirav18@gmail.com>
2021-08-12 16:53:44 +0530

    adding authentication functionality and flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,15 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require("semantic-ui-sass")
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
+  })
