be5c2ba9c9d6be7bd4d4c3f66f33565baa31e4ea
NiravDarji0612 <darjinirav18@gmail.com>
2021-08-16 18:40:20 +0530

    adding action cable functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
   before_action :logged_in_redirect, only: [:new, :create]
 
-  def new; end
+  def new 
+  end
 
   def create
     user = User.find_by(username: params[:session][:username])
